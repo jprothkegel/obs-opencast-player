9c182c10b21d3334d470df4e90cefa8cb331d658
jprothkegel <juan.rothkegel.12@sansano.usm.cl>
2019-07-15 10:31:32 -0400

    Modified: Initial page

--- src/router.js
+++ src/router.js
@@ -15,7 +15,8 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      redirect: '/ondemand'
     },
     {
       path: '/about',
